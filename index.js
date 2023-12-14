1c048402f05f388b1700ed534b33bad80bddcb63
JB <bradburyjesse7@gmail.com>
2023-12-14 11:14:15 -0700

    Application now genereates a readme with 2 prompts.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
 // TODO: Create an array of questions for user input
 const questions = [
     "What is the title of your project?",
@@ -20,26 +21,53 @@ const questions = [
     //             name: "languages",
     //         },
     //     ])
-        ];
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
+    const markdownContent = generateMarkdown(data);
+    fs.writeFile(
+        fileName,
+        markdownContent,
+        (err) => {
+            if (err) {
+                console.error(err);
+                return;
+            }
+            console.log("README.md Created!")
+            // `# CUSTOM README\n## Title\n${response.title}\n## Languages\n${response.description}\n## Methods of Communication\n${response.moc}`,
+            // () => {
+            //     console.log("README.md Created!");
+        }
+    );
 
     // TODO: I tried this but I actually think we will be using the generateMarkdown util to do this part, we may just call the function here. We probably need to add something like this:
-    // const generateMarkdown = require('./utils/generateMarkdown.js');
-
-    
-//     fs.writeFile(
-//         "README.md",
-//         `# CUSTOM README\n## Title\n${response.title}\n## Languages\n${response.description}\n## Methods of Communication\n${response.moc}`,
-//         () => {
-//           console.log("README.md Created!");
-//         }
-//       )
- }
+    // generateMarkdown.generateMarkdown();
+}
 
 // TODO: Create a function to initialize app
-function init() { }
+function init() {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                message: "What is your project title?",
+                name: "title",
+            },
+            {
+                type: "input",
+                message: "Write your description",
+                name: "description",
+            },
+
+        ])
+        .then((response) => {
+            const fileName = "README.md";
+            writeToFile(fileName, response)
+        }
+        );
+
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -13,8 +13,12 @@ function renderLicenseSection(license) { }
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  
+  ## Description:
+  ${data.description}
 
-`;
+  ### Thank You!
+  `;
 }
 
 module.exports = generateMarkdown;
